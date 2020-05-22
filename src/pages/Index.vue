3afd8fa9f2298fb5bc8cf91622d0e5312649e040
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-21 21:41:01 -0700

    Create search box foundation

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,9 +7,18 @@
       </div>
     </header>
     <section id="search">
-      <div class="w-full">
+      <div class="w-full flex content-center">
         <!-- Search box -->
-        <input class="w-full my-4 p-2 bg-white rounded-md p-4 shadow-lg" type="text" name="search" id="searchInput" placeholder="Search" v-on>
+        <input class="w-full my-4 p-2 bg-white rounded-md p-4 shadow-lg"
+        type="text"
+        name="search"
+        id="searchInput"
+        placeholder="Search"
+        v-model="searchQuery"
+        v-on:keypress="onKeyDown">
+        <button class="align-center bg-blue-200 p-4 rounded-md shadow-lg m-4" v-on:click="performSearch">
+          <i class="material-icons">search</i>
+        </button>
       </div>
     </section>
   </Layout>
@@ -19,6 +28,31 @@
 export default {
   metaInfo: {
     title: 'Home',
+  },
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
+  methods: {
+    onKeyDown(event) {
+      console.log(event);
+      if (event.key === 'Enter' && this.searchQuery.trim() !== '') {
+        this.performSearch();
+      }
+    },
+    performSearch() {
+      if (this.searchQuery.trim() === '') {
+        return;
+      }
+      // TODO: Update results on page
+      this.$router.push({
+        path: '/search',
+        query: {
+          q: this.searchQuery,
+        },
+      });
+    }
   },
 };
 </script>
---
+++ src/pages/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <main>
+      
+    </main>
+  </Layout>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
