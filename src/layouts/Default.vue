9959ddd696d3749386c6d7d4f6cc704bf12547aa
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-24 23:24:37 -0700

    Update default container layout styles

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
+  <div class="bg-gray-200">
+    <header class="header mx-auto max-w-3xl p-4">
+      <span class="inline-block text-2xl text-bold">
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+      </span>
+      <nav class="nav max-auto max-w-2xl">
+        <!-- TODO: Search bar -->
       </nav>
     </header>
-    <slot/>
+    <main class="mx-auto max-w-3xl p-4"> 
+      <slot></slot>
+    </main>
   </div>
 </template>
 
@@ -21,7 +22,7 @@ query {
 }
 </static-query>
 
-<style>
+<style lang="postcss" scoped>
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   margin:0;
@@ -29,22 +30,7 @@ body {
   line-height: 1.5;
 }
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
+  height: 64px;
 }
 </style>
