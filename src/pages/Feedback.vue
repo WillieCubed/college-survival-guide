4bd642af24baa2feb7e311688a39cfaf8770e366
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-24 23:24:14 -0700

    Begin creating feedback page
    This will handle suggested changes to guide content and contact

---
+++ src/pages/Feedback.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+    <main>
+      <section id="overview">
+        <h1 class="text-3xl">Overview</h1>
+        <p class="text-lg">Have any comments, complaints, or suggestions? I'm all ears.</p>
+        <p class="text-lg">
+          The College Survival Guide was built by a student, for students. If
+          you see any outdated or inaccurate information, please inform me. 
+        </p>
+        <p class="text-lg">
+          This guide has a built-in suggest feature. Simply highlight the
+          incorrect text, and you'll see an option to automagically fill out the
+          form below for your suggestion.
+        </p>
+        <p class="text-lg">
+          If you have any other concerns, email
+          <a
+            href="mailto:dev@williecubed.me?subject=College%20Survival%20Guide%20Feedback&body=Hey%20Willie,"
+          >dev@williecubed.me</a>, and I'll get back to you as soon as I can.
+        </p>
+        <p class="text-lg">
+          If you want to support the people (person) who made this, feel free to
+          donate:
+          <a href='https://ko-fi.com/williecubed' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi2.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+        </p>
+      </section>
+      <section id="form">
+        <h1 class="text-3xl">Guide Feedback Form</h1>
+      </section>
+    </main>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
